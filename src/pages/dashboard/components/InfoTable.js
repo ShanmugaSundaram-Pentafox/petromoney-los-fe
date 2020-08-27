ede2c33719831ce5f979513453ed5f72cc23b5ad
sriram10 <msriram1010@gmail.com>
2020-08-27 08:54:36 +0530

    login screen UI updated and favicon changed

--- src/pages/dashboard/components/InfoTable.js
+++ src/pages/dashboard/components/InfoTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
+import MUIDataTable from 'mui-datatables';
 
 const useStyles = makeStyles(theme => ({
   root: {

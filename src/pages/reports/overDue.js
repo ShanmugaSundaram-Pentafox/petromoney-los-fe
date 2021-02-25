163a82cd8757a27720048cdf8a2edfcd7e10775b
SivakCR <52414986+SivakCR@users.noreply.github.com>
2021-02-25 22:49:02 +0530

    added search filter print

--- src/pages/reports/overDue.js
+++ src/pages/reports/overDue.js
@@ -1,133 +1,114 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import { getReport } from '../../services/users.service';
+import React, { useMemo, useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import MUIDataTable from "mui-datatables";
 import { useMount } from 'react-use';
-import usePageTitle from '../../hooks/usePageTitle';
+import Paper from '@material-ui/core/Paper';
 import Currency from '../../components/Number/Currency';
-import moment from 'moment';
+import { getReport } from '../../services/users.service';
+import usePageTitle from '../../hooks/usePageTitle';
 
-const columns = [
-  { id: 'applicant_code', label: 'Applicant Code', minWidth: 170 },
-  { id: 'applicant_name', label: 'Applicant Name', minWidth: 100 },
-  {
-    id: 'cust_code',
-    label: 'Customer Code',
-    minWidth: 170,
-    align: 'right', 
-  },
-  {
-    id: 'cust_region',
-    label: 'Customer Region',
-    minWidth: 170,
-    align: 'right', 
-  },
-  {
-    id: 'duedate',
-    label: 'Due Date',
-    minWidth: 170,
-    align: 'right', 
-    format: (value) =>  <div>{ moment(new Date(value)).format('DD MMM, YYYY') }</div>
-  },
-  {
-    id: 'disb_amt',
-    label: 'disburse Amt',
-    minWidth: 170,
-    align: 'right', 
-    format: (value) => <Currency value={value}/>,
-  },
-  {
-    id: 'tot_due',
-    label: 'Total Due',
-    minWidth: 170,
-    align: 'right', 
-    format: (value) => <Currency value={value}/>,
-  },
-];
-
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
   },
-  container: {
-    maxHeight: '100%',
+  title: {
+    fontWeight: 500
   },
-});
+  pill: {
+    display: 'inline-block',
+    borderRadius: '29px',
+    padding: '3px 8px',
+    fontSize: '13px',
+    fontWeight: '600',
+    minWidth: '30px',
+    textAlign: 'center',
+  },
+  pills_FUEL: {
+    color: '#d35178',
+    backgroundColor: '#f7eae8'
+  },
+  pills_SOLAR: {
+    color: '#51b37f',
+    backgroundColor: '#e1f8e5',
+  }
+}));
 
-export default function StickyHeadTable() {
-  usePageTitle('Report OverDue')
-  const [rows , setRows]= useState([])
+const OverDueTable = () => {
+  const classes = useStyles();
+
+  const [loans , setLoans]= useState([])
   useMount( async () => {
       var a = await getReport()
-      setRows(a.overdue)
+      setLoans(a.overdue)
       console.log(a.overdue)
   })
+  usePageTitle('Due Report')
+  const columns = useMemo(() => {
+    return [
+      { name: 'applicant_code', label: 'Applicant Code'  },
+      { name: 'applicant_name', label: 'Applicant Name', minWidth: 100 },
+      {
+        name: 'cust_code',
+        label: 'Customer Code',
+        minWidth: 170,
+        align: 'right', 
+      },
+      {
+        name: 'cust_region',
+        label: 'Customer Region',
+        minWidth: 170,
+        align: 'right', 
+      },
+      {
+        name: 'penal_overdue',
+        label: 'Penal Overdue', 
+        options: { 
+          filter: false,
+          sort: true,
+          customBodyRender: value => {
+            return <Currency value={value}/>
+          }
+        }
+      },
+      {
+        name: 'duedate',
+        label: 'Due Date', 
+        options: { 
+          filter: false, 
+        }
+      },
+      {
+        name: 'tot_due',
+        label: 'Total Due', 
+        options: { 
+          filter: false,
+          sort: true,
+          customBodyRender: value => {
+            return <Currency value={value}/>
+          }
+        }
+      },
+    ]
+  }, []);
 
-
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(25);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  const options = { 
+    selectableRowsHeader: false,
+    selectableRows: 'none', 
   };
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[25, 50, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
+    <div className={classes.root}>
+      {
+        Array.isArray(loans) && loans.length ? (
+          <MUIDataTable
+            title={"OVER DUE"}
+            data={loans}
+            columns={columns}
+            options={options}
+          />
+        ) : <Paper style={{ padding: 10 }}>No Disbursed Loans</Paper> 
+      }
+    </div>
+  )
 }
+
+export default OverDueTable 

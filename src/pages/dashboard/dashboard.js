46af73746a0c215f4670f4ffca78eddad79a6839
madhumathi0213 <madhumathi@pentafox.in>
2021-03-08 18:18:24 +0530

    loan stats order changed in dashboard and loans page

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -51,10 +51,10 @@ const Dashboard = ({ currentUser, dashboardView }) => {
         let cdata = [
           { name: 'Submitted', count: data.submitted_count },
           { name: 'Pending Approval', count: data.loan_approval_count || 0 },
-          { name: 'Pending Disbursement Approval', count: data.disbursement_approval_count || 0 },
           { name: 'Approved', count: data.approved_count },
-          { name: 'Rejected', count: data.rejected_count },
+          { name: 'Pending Disbursement Approval', count: data.disbursement_approval_count || 0 },
           { name: 'Disbursed', count: data.disbursed_count },
+          { name: 'Rejected', count: data.rejected_count },
         ];
         setChartData(cdata);
       })

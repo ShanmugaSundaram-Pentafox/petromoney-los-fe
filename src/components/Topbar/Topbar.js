059a545cc40486f57799a9657bd9e020e37d84e4
madhumathi0213 <madhumathi@pentafox.in>
2021-03-30 13:17:42 +0530

    added logout icon in topbar

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -150,6 +150,21 @@ const Topbar = (props) => {
 
           </h2>
           <div className={classes.flexGrow} />
+          <Hidden mdDown>
+            {/* <Searchbox /> */}
+            {/* <NotificationsBell action={() => setShowNotificationSidebar(true)} /> */}
+            <LoginUserInfo user={user} logout={logout} />
+  
+            {/* <Tooltip title="Logout">
+              <IconButton
+                className={classes.signOutButton}
+                color="inherit"
+                onClick={logout}
+              >
+                <InputIcon />
+              </IconButton>
+            </Tooltip> */}
+          </Hidden>
           <Hidden lgUp>
             {/* <Searchbox /> */}
             {/* <NotificationsBell action={() => setShowNotificationSidebar(true)} /> */}

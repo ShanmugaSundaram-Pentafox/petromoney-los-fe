0d227b6574efb2233572ed58b823a7780cf4dd69
madhumathi0213 <madhumathi@pentafox.in>
2021-05-25 10:06:30 +0530

    changed toogle button view

--- src/pages/dealershipDetails/components/DealerEditForm.js
+++ src/pages/dealershipDetails/components/DealerEditForm.js
@@ -329,10 +329,10 @@ const DealerEditForm = ({ modelType, data, dealersList, deleteFile, editableValu
                 </Grid>
                 <Grid {...gridItem} md={6}>
                     <Typography component="div">
-                        <Grid component="label" container alignItems="center" spacing={2}>
-                            <Grid item md={12}>Is mobile on whatsapp ?</Grid>
-                            <Grid item>No</Grid>
-                            <Grid item>
+                        <Grid component="label" container alignItems="center" style={{marginBottom:'10px',marginTop:'6px'}} spacing={2}>
+                            <Grid md={12} style={{paddingLeft:'8px'}}>Mobile number on Whatsapp?</Grid>
+                            <Grid style={{paddingLeft:'8px'}}>No</Grid>
+                            <Grid>
                                 <Switch
                                     checked={state.checkedA}
                                     onChange={handleChange}
@@ -341,16 +341,16 @@ const DealerEditForm = ({ modelType, data, dealersList, deleteFile, editableValu
                                     inputProps={{ 'aria-label': 'secondary checkbox' }}
                                 />
                             </Grid>
-                            <Grid item>Yes</Grid>
+                            <Grid>Yes</Grid>
                         </Grid>
                     </Typography>
                 </Grid>
-                <Grid {...gridItem} md={6}>
-                    <Typography component="div">
-                        <Grid component="label" container alignItems="center" spacing={2}>
-                            <Grid item md={12}>Is pan linked to aadhar ?</Grid>
-                            <Grid item>No</Grid>
-                            <Grid item>
+                <Grid item md={6}>
+                    <Typography component="div" >
+                        <Grid component="label" container style={{marginBottom:'8px',marginTop:'6px'}} alignItems="center" spacing={2}>
+                            <Grid md={12} style={{paddingLeft:'8px'}}>PAN linked to AADHAR?</Grid>
+                            <Grid style={{paddingLeft:'8px'}}>No</Grid>
+                            <Grid>
                                 <Switch
                                     checked={state.checkedB}
                                     onChange={handleChange}
@@ -359,7 +359,7 @@ const DealerEditForm = ({ modelType, data, dealersList, deleteFile, editableValu
                                     inputProps={{ 'aria-label': 'secondary checkbox' }}
                                 />
                             </Grid>
-                            <Grid item>Yes</Grid>
+                            <Grid>Yes</Grid>
                         </Grid>
                     </Typography>
                 </Grid>

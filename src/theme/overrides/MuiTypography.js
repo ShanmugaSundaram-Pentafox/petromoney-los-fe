f7dd735b04d21c7c39b8d2477e28684c54f7ce31
sriram1010 <msriram1010@gmail.com>
2021-08-05 12:42:17 +0530

    updated region mapping

--- src/theme/overrides/MuiTypography.js
+++ src/theme/overrides/MuiTypography.js
@@ -1,6 +1,13 @@
+import typography from "../typography";
+
 export default {
   fontWeightMedium: 600,
   gutterBottom: {
     marginBottom: 8
-  }
+  },
+  root: {
+    '&.MuiFormControlLabel-label': {
+      ...typography.body2
+    }
+  },
 };

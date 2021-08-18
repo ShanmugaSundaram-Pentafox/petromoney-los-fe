9aecbce8785887d37f2cf707c66ea80fe25faf8f
Shidiq Ahmed <shidiq@pentafox.in>
2021-08-18 16:30:43 +0530

    master data ui changes

--- src/pages/settings/components/MasterDataTable.js
+++ src/pages/settings/components/MasterDataTable.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Button, Drawer, Typography } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
@@ -8,10 +8,8 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
-import InputAdornment from "@material-ui/core/InputAdornment";
+import InputAdornment from '@material-ui/core/InputAdornment';
 import { Tooltip } from '@material-ui/core';
-
-
 
 const useStyles = makeStyles({
   root: {
@@ -41,97 +39,149 @@ const useStyles = makeStyles({
     justifyContent: 'space-between',
     padding: 6,
     alignItems: 'center',
-    '&:hover':{
+    '&:hover': {
       backgroundColor: '#EEEEEE',
-    }
+      '& $btn': {
+        visibility: 'visible',
+      },
+    },
   },
   divider: {
-    backgroundColor: '#EEEEEE'
+    backgroundColor: '#EEEEEE',
   },
   drawer: {
-    position: 'absolute'
+    position: 'absolute',
   },
   btn: {
+    visibility: 'hidden',
+    color: '#687980',
+  },
+  nodata: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: 380,
+    alignItems: 'center'
     
   }
 });
 
-
-function Contain({title, data, label}) {
+function Contain({ title, data, label }) {
   const classes = useStyles();
   const [value, setValue] = useState();
   const [openEditForm, setOpenEditForm] = useState(false);
-  console.log(value);
+  // console.log(value);
+
+  const filteredData = data.filter((item) => item.name.toUpperCase().includes(value?.toUpperCase()))
 
   return (
     <>
       <Paper className={classes.root}>
         <div className={classes.title}>
-          <Typography variant='h5' style={{marginLeft: 5}}>{title}</Typography>
+          <Typography variant='h5' style={{ marginLeft: 5 }}>
+            {title}
+          </Typography>
+          <Tooltip title={'Add '+title}>
           <Button variant='contained' color='primary'>
             ADD
           </Button>
+          </Tooltip>
         </div>
-        <form className={classes.search} noValidate autoComplete="off">
-          <TextField id="search" variant="outlined" InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon fontSize='small'/>
-            </InputAdornment>
-          ),
-        }} margin='normal' fullWidth style={{marginTop: 10}} value={value} onChange={e => setValue(e.target.value)}/>
+        <form className={classes.search} noValidate autoComplete='off'>
+          <TextField
+            id='search'
+            variant='outlined'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <SearchIcon fontSize='small' />
+                </InputAdornment>
+              ),
+            }}
+            margin='normal'
+            fullWidth
+            style={{ marginTop: 10 }}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </form>
         <div className={classes.section}>
-          {
-            value? (
-              data.filter((item) => item.name.includes(value?.toUpperCase()))
-          .map((data, i) => {
-            console.log(data);
-            return (
-              <>
-              <div className={classes.label}>
-                <Typography variant='h7' style={{paddingLeft: 18}}>{data.name}</Typography>
-                <div>
-                  <Tooltip title='Edit'>
-                  <IconButton className={classes.btn} size='small'><EditIcon fontSize='small'/></IconButton>
-                  </Tooltip>
-                  <Tooltip title='Delete'>
-                  <IconButton className={classes.btn} size='small'><DeleteIcon fontSize='small'/></IconButton>
-                  </Tooltip>
+          {value
+            ? filteredData.length > 0?
+
+                filteredData.map((item, i) => {
+                  return (
+                    <>
+                      <div className={classes.label}>
+                        <Typography variant='h7' style={{ paddingLeft: 18 }}>
+                          {item.name}
+                        </Typography>
+                        <div>
+                          <Tooltip title='Edit'>
+                            <IconButton className={classes.btn} size='small'>
+                              <EditIcon fontSize='small' />
+                            </IconButton>
+                          </Tooltip>
+                          <Tooltip title='Delete'>
+                            <IconButton className={classes.btn} size='small'>
+                              <DeleteIcon fontSize='small' />
+                            </IconButton>
+                          </Tooltip>
+                        </div>
+                      </div>
+                      <Divider className={classes.divider} />
+                    </>
+                  );
+                }) : 
+                <>
+                <div className={classes.nodata}>
+                  <Typography variant='h6'>No Data Found</Typography>
+                  <div>
+                  <Button variant='outlined' size='small' color='primary' style={{marginTop: 15}}>
+                    ADD
+                  </Button>
+                  </div>
                 </div>
-              </div>
-              <Divider className={classes.divider}/>
-              </>
-            );
-          })
-            ) : (
-              data.map((data, i) => {
+                </>
+            : data.map((item, i) => {
                 return (
                   <>
-                  <div className={classes.label}>
-                    <Typography variant='h7' style={{paddingLeft: 18}}>{data.name? data.name : data.region}</Typography>
-                    <div>
-                      <Tooltip title='Edit'>
-                      <IconButton className={classes.btn} size='small'><EditIcon fontSize='small'/></IconButton>
-                      </Tooltip>
-                      <Tooltip title='Delete'>
-                      <IconButton className={classes.btn} size='small'><DeleteIcon fontSize='small'/></IconButton>
-                      </Tooltip>
+                    <div className={classes.label}>
+                      <Typography variant='h7' style={{ paddingLeft: 18 }}>
+                        {item.name ? item.name : item.region}
+                      </Typography>
+                      <div>
+                        <Tooltip title='Edit'>
+                          <IconButton className={classes.btn} size='small'>
+                            <EditIcon
+                              fontSize='small'
+                              className={classes.edt}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title='Delete'>
+                          <IconButton className={classes.btn} size='small'>
+                            <DeleteIcon
+                              fontSize='small'
+                              className={classes.del}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                      </div>
                     </div>
-                  </div>
-                  <Divider className={classes.divider}/>
+                    <Divider className={classes.divider} />
                   </>
                 );
-              })
-            )  
-        }
+              })}
         </div>
-        <Drawer 
-        anchor='bottom'
-        className={classes.drawer}
-        variant='temporary'
-        open={openEditForm}
-        >Hello</Drawer>
+        <Drawer
+          anchor='bottom'
+          className={classes.drawer}
+          variant='temporary'
+          open={openEditForm}
+        >
+          Hello
+        </Drawer>
       </Paper>
     </>
   );

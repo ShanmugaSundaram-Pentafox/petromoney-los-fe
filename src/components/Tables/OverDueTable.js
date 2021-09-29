1f673abb71601de95cbc395a6c7fa45088d8f414
madhumathi_allimuthu <madhumathi@pentafox.in>
2021-09-29 18:38:30 +0530

    Due / overdue reports for dealer login

--- src/components/Tables/OverDueTable.js
+++ src/components/Tables/OverDueTable.js
@@ -4,102 +4,109 @@ import MUIDataTable from "mui-datatables";
 import { useMount } from 'react-use';
 import Paper from '@material-ui/core/Paper';
 import Currency from '../../components/Number/Currency';
-import { getReport } from '../../services/users.service';
-import usePageTitle from '../../hooks/usePageTitle';
-import { Grid } from '@material-ui/core';
-import Skeleton from '@material-ui/lab/Skeleton';
+import { getTestReport } from '../../services/users.service';
 import moment from 'moment';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { Grid } from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        paddingBottom: 20
-    },
-    title: {
-        fontWeight: 500
-    },
-    pill: {
-        display: 'inline-block',
-        borderRadius: '29px',
-        padding: '3px 8px',
-        fontSize: '13px',
-        fontWeight: '600',
-        minWidth: '30px',
-        textAlign: 'center',
-    },
-    pills_FUEL: {
-        color: '#d35178',
-        backgroundColor: '#f7eae8'
-    },
-    pills_SOLAR: {
-        color: '#51b37f',
-        backgroundColor: '#e1f8e5',
-    }
+  root: {
+    paddingBottom: 20
+  },
+  title: {
+    fontWeight: 500
+  },
+  pill: {
+    display: 'inline-block',
+    borderRadius: '29px',
+    padding: '3px 8px',
+    fontSize: '13px',
+    fontWeight: '600',
+    minWidth: '30px',
+    textAlign: 'center',
+  },
+  pills_FUEL: {
+    color: '#d35178',
+    backgroundColor: '#f7eae8'
+  },
+  pills_SOLAR: {
+    color: '#51b37f',
+    backgroundColor: '#e1f8e5',
+  }
 }));
-const OverDueTable = ({ onRowClick }) => {
+const OverDueTable = ({ id, onRowClick }) => {
 
-    const classes = useStyles();
-    const [loans, setLoans] = useState([])
-    useMount(async () => {
-        var a = await getReport()
-        setLoans(a.overdue)
-    })
-    const columns = useMemo(() => {
-        return [
-            { name: 'prospectcode', label: 'Loan ID' },
-            { name: 'applicant_code', label: 'Applicant Code' },
-            { name: 'applicant_name', label: 'Applicant Name' },
-            { name: 'cust_code', label: 'Dealership ID' },
-            { name: 'cust_region', label: 'Customer Region' },
-            {
-                name: 'duedate',
-                label: 'Due Date',
-                options: {
-                    filter: false,
-                },
-                customBodyRender: value => {
-                    return <div>
-                        {value ? moment(new Date(value)).format('DD-MM-YYYY') : '-'}
-                    </div>
-                }
-            },
-            {
-                name: 'tot_due',
-                label: 'Total Due',
-                options: {
-                    filter: false,
-                    sort: true,
-                    customBodyRender: value => {
-                        return <Currency value={value} />
-                    }
-                }
-            },
-        ]
-    }, []);
-    const options = {
-        selectableRowsHeader: false,
-        selectableRows: 'none',
-        isRowSelectable: () => true,
-        onRowClick: (rowData, { dataIndex }) => {
-            onRowClick(loans[dataIndex].dealership_id, loans[dataIndex])
+  const classes = useStyles();
+  const [loans, setLoans] = useState([])
+  const [loading, setLoading] = useState(false);
+  useMount(async () => {
+    getTestReport(id)
+      .then((data) => {
+        setLoans(data.overdue)
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log(e);
+      });
+  })
+  const columns = useMemo(() => {
+    return [
+      { name: 'prospectcode', label: 'Loan ID' },
+      {
+        name: 'duedate',
+        label: 'Due Date',
+        options: {
+          filter: false,
         },
-        rowsPerPage: 15,
-        rowsPerPageOptions: [15, 20, 30],
-    };
-    return (
-        <div className={classes.root}>
-            {
-                Array.isArray(loans) && loans.length ? (
-                    <MUIDataTable
-                        title={"Over Due Reports"}
-                        data={loans}
-                        columns={columns}
-                        options={options}
-                    />
-                ) : <Paper style={{ marginTop: 10, padding: 10 }}>No Over Due Reports</Paper>
-            }
-        </div>
-    )
+        customBodyRender: value => {
+          return <div>
+            {value ? moment(new Date(value)).format('DD-MM-YYYY') : '-'}
+          </div>
+        }
+      },
+      {
+        name: 'tot_due',
+        label: 'Total Due',
+        options: {
+          filter: false,
+          sort: true,
+          customBodyRender: value => {
+            return <Currency value={value} />
+          }
+        }
+      },
+    ]
+  }, []);
+  const options = {
+    selectableRowsHeader: false,
+    selectableRows: 'none',
+    isRowSelectable: () => true,
+    onRowClick: (rowData, { dataIndex }) => {
+      onRowClick(loans[dataIndex].dealership_id, loans[dataIndex])
+    },
+    rowsPerPage: 15,
+    rowsPerPageOptions: [15, 20, 30],
+  };
+  return (
+    <div className={classes.root}>
+      {
+        loading ? (
+          <Grid item xs={12}>
+            <Skeleton variant="rect" width="100%" height={400} />
+          </Grid>
+        ) : Array.isArray(loans) ? (
+          <MUIDataTable
+            title={"Overdue Reports"}
+            data={loans}
+            columns={columns}
+            options={options}
+          />
+        ) : <Paper style={{ marginTop: 10, padding: 10 }}>No overdue Reports found</Paper>
+      }
+    </div>
+  )
 }
 
 export default OverDueTable;

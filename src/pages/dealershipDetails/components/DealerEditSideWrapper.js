a89b86ff42f99fbcbbe6280b2e12be3bde118bfb
madhumathi_allimuthu <madhumathi@pentafox.in>
2021-09-30 11:14:41 +0530

    payload validation for dealers/co applicants/guarantors

--- src/pages/dealershipDetails/components/DealerEditSideWrapper.js
+++ src/pages/dealershipDetails/components/DealerEditSideWrapper.js
@@ -21,6 +21,19 @@ import { useSnackbar } from 'notistack';
 import CloseIcon from '@material-ui/icons/Close';
 import { format, parse } from 'date-fns';
 
+
+const checkChanges = (originalData, editedData) => {
+  let object = {};
+  for (const [originalDatakey, originalDatavalue] of Object.entries(originalData)) {
+    for (const [editedDatakey, editedDatavalue] of Object.entries(editedData)) {
+      if (originalDatakey == editedDatakey && originalDatavalue != editedDatavalue) {
+        object[editedDatakey] = editedDatavalue;
+      }
+    }
+  }
+  return object;
+}
+
 const useStyles = makeStyles((theme) => ({
   sidePanelTitle: {
     // textAlign: 'center',
@@ -198,9 +211,13 @@ const DealerEditSideWrapper = ({
       setLoading(true);
       const dob = selectedDate ? format(new Date(selectedDate), "dd-MM-yyyy") : values.dob
       const date_values = { ...values, dob: dob, pan: values.pan.toUpperCase(), is_whatsapp: selectedState.checkedA === true ? 1 : 0, is_aadhar_linked: selectedState.checkedB === true ? 1 : 0 };
-      const data = new FormData();
-      Object.keys(date_values).forEach((key) => {
-        data.append(key, date_values[key]);
+      let obj = {};
+      if (values.id) {
+        obj = checkChanges(data, date_values)
+      }
+      const formData = new FormData();
+      Object.keys(obj).forEach((key) => {
+        formData.append(key, obj[key]);
       });
       const apiURL =
         modelType === 'DEALER'
@@ -212,13 +229,12 @@ const DealerEditSideWrapper = ({
       if (values.id) {
         url += `/${values.id}`;
       }
-
       if (modelType !== 'GUARANTOR') {
-        data.append('user_id', currentUser.id);
+        formData.append('user_id', currentUser.id);
       }
       fetch(`${URL.base}${url}`, {
         method: 'POST',
-        body: data,
+        body: formData,
         headers: {
           Authorization: `Bearer ${currentUser.token}`,
         },
@@ -236,7 +252,11 @@ const DealerEditSideWrapper = ({
             },
             variant: 'success',
           });
-          setApiCallMessage(isAdd ? 'Dealer Added' : 'Dealer Updated');
+
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+          // setApiCallMessage(isAdd ? 'Dealer Added' : 'Dealer Updated');
           onClose();
           modelType === 'DEALER'
             ? getDealerApiCall(dealershipId)
@@ -252,8 +272,8 @@ const DealerEditSideWrapper = ({
             },
             variant: 'error',
           });
-          setApicallStatus('error');
-          setApiCallMessage('Sorry! Unable to add or Update. Try again later.');
+          // setApicallStatus('error');
+          // setApiCallMessage('Sorry! Unable to add or Update. Try again later.');
           logger(err);
         });
     },

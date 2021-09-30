eb6274a9e0cf8c7474a176db0740e16adf4e9a55
madhumathi_allimuthu <madhumathi@pentafox.in>
2021-09-30 11:55:18 +0530

    common object fix

--- src/utils/compareObject.util.js
+++ src/utils/compareObject.util.js
@@ -1,4 +1,4 @@
-export const compareObject = (originalData, editedData, commonObj) => {
+export const compareObject = (originalData, editedData, commonObj = {}) => {
     let object = { ...commonObj };
     for (const [originalDatakey, originalDatavalue] of Object.entries(originalData)) {
         for (const [editedDatakey, editedDatavalue] of Object.entries(editedData)) {

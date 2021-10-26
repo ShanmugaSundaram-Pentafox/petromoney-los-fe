54e00b3a2bb69d85dbcdb4bf43e5cffabd18bff1
madhumathi_allimuthu <madhumathi@pentafox.in>
2021-10-26 18:32:48 +0530

    added monthwise sales data table

--- src/pages/dashboard/components/MonthlySalesInfo.js
+++ src/pages/dashboard/components/MonthlySalesInfo.js
@@ -7,39 +7,15 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
-import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
-import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
-import Currency from '../../../components/Number/Currency';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import Popover from '@material-ui/core/Popover';
 import { makeStyles } from '@material-ui/core';
-import { getDealershipSalesById, postDealershipSalesById } from '../../../services/dealerships.service';
+import { getDealershipSalesById, postDealershipMonthlySalesById, updateDealershipMonthlySalesById } from '../../../services/dealerships.service';
 import TextInput from '../../../components/TextInput/TextInput';
 import UserCan, { permissionCheck } from '../../../components/UserCan/UserCan';
 import { rulesList } from '../../../config/userRules';
 import { useSnackbar } from "notistack";
-import MonthlySalesInfo from './MonthlySalesInfo';
-
-
-/**
-{
-  "dealership_id": 15257010,
-  "from_year": 2016,
-  "hsd": 593.77,
-  "hsd_rs": 930.03,
-  "ms": 729.0,
-  "ms_rs": 1343.0,
-  "tmf": 1523.8,
-  "to_year": 2017
-}
- */
 
 
 const useStyles = makeStyles(theme => ({
-  popover: {
-    pointerEvents: 'none',
-  },
   paper: {
     padding: theme.spacing(1),
   },
@@ -62,38 +38,34 @@ const SalesInfoWrapper = styled.div`
   padding-top: 8px;
   margin-bottom:10px;
   overflow-y:auto;
-  h5 {
-    // margin-top: 12px;
-    // font-size: 18px;
-  }
+  
 `;
 
 const SalesTableWrapper = styled.div`
   display: flex;
   padding: 4px 0;
   flex-direction: ${props => props.column ? 'column' : 'row'};
-
-  > div {
-    &:last-child {
-      // flex: 1;
-      // padding-left: 12px;
-    }
-  }
 `;
 
-const SalesInfo = ({
-  id,
-  titleAlign,
-  column,
-  currentUser
-}) => {
+const getPastFiveYears = () => {
+  const LastFiveYear = []
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  for (let i = 0; i < 5; i++) {
+    LastFiveYear.push(currentYear - i)
+  }
+  return LastFiveYear;
+}
+
+
+const MonthlySalesInfo = ({ id, titleAlign, column, currentUser }) => {
   const [info, setInfo] = useState([]);
   const classes = useStyles();
   const [addNewRow, setAddNewRow] = useState();
   const [apiData, setApiData] = useState({});
-  const [anchorEl, setAnchorEl] = useState(null);
   const [editRow, setEditRow] = useState({});
   const { enqueueSnackbar } = useSnackbar();
+  const LastFiveYear = getPastFiveYears()
 
   useEffect(() => {
     if (id) {
@@ -103,14 +75,6 @@ const SalesInfo = ({
     }
   }, [id]);
 
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
   const onTextChange = e => {
     const { name, value } = e.target;
     setApiData({
@@ -119,57 +83,31 @@ const SalesInfo = ({
     })
   }
 
-  const open = Boolean(anchorEl);
 
   const saveNewSalesData = () => {
-    if (parseInt(apiData.from_year) < 1900 || parseInt(apiData.from_year) >= parseInt(apiData.to_year)) {
-      enqueueSnackbar("Year error, Please check...", {
-        anchorOrigin: {
-          vertical: "top",
-          horizontal: "right",
-        },
-        variant: "error",
-      });
-    }
-    else {
-      if (Object.keys(apiData).length < 4) return null;
-      const objBody = {
-        user_id: currentUser.id, ...apiData
-      }
-      postDealershipSalesById(id, objBody)
-        .then(res => {
-          setInfo(res);
-          setAddNewRow(false);
-        })
-        .catch(err => {
-          console.log('Sales data save error - ', err);
-        })
-
-    }
+    if (Object.keys(apiData).length < 4) return null;
+    postDealershipMonthlySalesById(id, apiData)
+      .then(res => {
+        setInfo(res);
+        setAddNewRow(false);
+      })
+      .catch(err => {
+        console.log('Sales data save error - ', err);
+      })
   }
 
   const saveEditRow = (data, i) => {
-    if (parseInt(data.from_year) < 1900 || parseInt(data.from_year) >= parseInt(data.to_year)) {
-      enqueueSnackbar("Year error, Please check...", {
-        anchorOrigin: {
-          vertical: "top",
-          horizontal: "right",
-        },
-        variant: "error",
-      });
-    } else {
-      const objBody = {
-        user_id: currentUser.id, ...data
-      }
-      postDealershipSalesById(id, objBody)
-        .then(res => {
-          setInfo(res);
-          setEditRow({});
-        })
-        .catch(err => {
-          console.log('Sales data save error - ', err);
-        })
+    const objBody = {
+      user_id: currentUser.id, ...data
     }
+    updateDealershipMonthlySalesById(id, objBody)
+      .then(res => {
+        setInfo(res);
+        setEditRow({});
+      })
+      .catch(err => {
+        console.log('Sales data save error - ', err);
+      })
   }
 
   const editSalesRow = (rowData, rowIndex) => {
@@ -183,42 +121,11 @@ const SalesInfo = ({
       [name]: value
     })
   }
-
-  // if(Array.isArray(info) && !info.length)
-  //   return (
-  //     <SalesInfoWrapper>
-  //       <Typography align={titleAlign} variant="h5">Sales data not available</Typography>
-  //       <div style={{ textAlign: 'center', marginTop: 8, paddingBottom: 8 }}>
-  //         <Button color="primary" variant="contained" size="small" onClick={() => null}>Add Sales Data</Button>
-  //       </div>
-  //     </SalesInfoWrapper>
-  //   );
   const editable = permissionCheck(currentUser.role_name, rulesList.dealership_edit)
   return (
     <SalesInfoWrapper>
-      <Popover
-        id="mouse-over-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography variant='subtitle2' component='div'>The combined value of MS and HSD are considered</Typography>
-      </Popover>
       <div className={classes.title}>
-        <Typography align={titleAlign} variant="h5">Sales History</Typography>
+        <Typography align={titleAlign} variant="h5">Sales History (Month-wise)</Typography>
         <UserCan
           role={currentUser.role_name}
           perform={rulesList.dealership_edit}
@@ -236,9 +143,7 @@ const SalesInfo = ({
               <TableRow>
                 <TableCell>Sales Data(in KL)</TableCell>
                 <TableCell align="center">MS</TableCell>
-                {/* <TableCell align="center">MS Gross</TableCell> */}
                 <TableCell align="center">HSD</TableCell>
-                {/* <TableCell align="center">HSD Gross</TableCell> */}
                 <TableCell align="right">Total (in KL)</TableCell>
                 <TableCell align="right">Action</TableCell>
               </TableRow>
@@ -249,22 +154,22 @@ const SalesInfo = ({
                   <TableRow key={`edit-row-${i}`}>
                     <TableCell scope="row" component="th">
                       <TextInput
-                        fullWidth={false}
-                        label="From Year"
-                        name="from_year"
+                        fullWidth={true}
+                        label="Month"
+                        name="month"
                         type="number"
                         disabled={true}
-                        value={editRow.from_year}
+                        value={editRow.year}
                         onChange={onEditTextChange}
                       />
                       -
                       <TextInput
-                        fullWidth={false}
-                        label="To Year"
-                        name="to_year"
+                        fullWidth={true}
+                        label="Year"
+                        name="year"
                         type="number"
                         disabled={true}
-                        value={editRow.to_year}
+                        value={editRow.year}
                         onChange={onEditTextChange}
                       />
                     </TableCell>
@@ -309,24 +214,9 @@ const SalesInfo = ({
                   </TableRow>
                 ) : (
                   <TableRow key={i}>
-                    <TableCell scope="row" component="th">{row.from_year} - {row.to_year}
-                      {/* {row.to_year >= 2020 ? <InfoOutlinedIcon
-                        className={classes.infoIcon}
-                        color='primary'
-                        aria-haspopup="true"
-                        aria-owns={open ? 'mouse-over-popover' : undefined}
-                        onMouseEnter={handlePopoverOpen}
-                        onMouseLeave={handlePopoverClose} />
-                        : null} */}
-                    </TableCell>
-                    {/* {row.to_year >= 2020 ?
-                      <TableCell align="center" colSpan={2}>{row.ms?.toFixed(2)}</TableCell>
-                      : <> */}
+                    <TableCell scope="row" component="th">{row.month} - {row.year}</TableCell>
                     <TableCell align="right">{row.ms?.toFixed(2)}</TableCell>
-                    {/* <TableCell align="right">{row.ms_gross?.toFixed(2)}</TableCell> */}
                     <TableCell align="right">{row.hsd?.toFixed(2)}</TableCell>
-                    {/* <TableCell align="right">{row.hsd_gross?.toFixed(2)}</TableCell> */}
-                    {/* </>} */}
                     <TableCell align="right">{(row.ms + row.hsd)?.toFixed(2)}</TableCell>
                     <TableCell align="right">
                       {
@@ -351,22 +241,44 @@ const SalesInfo = ({
                   <TableRow key={"new-row"}>
                     <TableCell scope="row" component="th">
                       <TextInput
-                        fullWidth={false}
-                        label="From Year"
-                        name="from_year"
+                        select
+                        fullWidth={true}
+                        label="Month"
+                        name="month"
                         type="number"
-                        value={apiData.from_year}
+                        value={apiData.month}
                         onChange={onTextChange}
-                      />
+                      >
+                        <option value=" ">Choose month</option>
+                        <option value="1">January</option>
+                        <option value="2">Feburary</option>
+                        <option value="3">March</option>
+                        <option value="4">Apirl</option>
+                        <option value="5">May</option>
+                        <option value="6">June</option>
+                        <option value="7">July</option>
+                        <option value="8">August</option>
+                        <option value="9">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                      </TextInput>
                       -
                       <TextInput
-                        fullWidth={false}
-                        label="To Year"
-                        name="to_year"
+                        select
+                        fullWidth={true}
+                        label="Year"
+                        name="year"
                         type="number"
-                        value={apiData.to_year}
+                        value={apiData.year}
                         onChange={onTextChange}
-                      />
+                      >
+                        {
+                          LastFiveYear.map(item => {
+                            return <option value={item}>{item}</option>
+                          })
+                        }
+                      </TextInput>
                     </TableCell>
                     <TableCell align="right">
                       <TextInput
@@ -395,82 +307,24 @@ const SalesInfo = ({
                         onClick={() => {
                           setAddNewRow(false);
                         }}>
-                          Cancel
+                        Cancel
                       </Button>
                       <Button
                         size="small"
                         variant="outlined"
                         color="error"
-                        // className={classes.btnSuccess}
                         onClick={saveNewSalesData}>
-                          Save
+                        Save
                       </Button>
                     </TableCell>
                   </TableRow>
-
                 )
               }
             </TableBody>
           </Table>
-          <MonthlySalesInfo currentUser={currentUser} id={id} titleAlign={titleAlign} column={column} />
-
-          {/* <UserCan
-            role={currentUser.role_name}
-            perform={rulesList.dealership_edit}
-            yes={() => (
-              <div style={{ textAlign: 'right', marginTop: 8 }}>
-                <Button color="primary" variant="contained" size="small" onClick={() => setAddNewRow(true)}>Add Sales Data</Button>
-              </div>
-            )}
-          /> */}
         </div>
-        {/* <div className={classes.table}>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Sales Data(in Lakhs)</TableCell>
-                <TableCell align="center">MS</TableCell>
-                <TableCell align="center">MS Gross</TableCell>
-                <TableCell align="center">HSD</TableCell>
-                <TableCell align="center">HSD Gross</TableCell>
-                <TableCell align="right">Total (in Lakhs)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {
-                info.map((row, i) => (
-                  <TableRow key={i}>
-                    <TableCell scope="row" component="th">{row.from_year} - {row.to_year}
-                      {
-                        // row.to_year >= 2020 ? <InfoOutlinedIcon
-                        // className={classes.infoIcon}
-                        // color='primary'
-                        // aria-haspopup="true"
-                        // aria-owns={open ? 'mouse-over-popover' : undefined}
-                        // onMouseEnter={handlePopoverOpen}
-                        // onMouseLeave={handlePopoverClose} />
-                        // : null
-                      }
-                    </TableCell>
-                    {row.to_year >= 2020 ?
-                      <TableCell align="center" colSpan={2}><Currency value={row.ms_rs?.toFixed(2)} /></TableCell>
-                      : <>
-                        <TableCell align="right"><Currency value={row.ms_rs?.toFixed(2)} /></TableCell>
-                        <TableCell align="right"><Currency value={row.ms_gross?.toFixed(2)} /></TableCell>
-                        <TableCell align="right"><Currency value={row.hsd_rs?.toFixed(2)} /></TableCell>
-                        <TableCell align="right"><Currency value={row.hsd_gross?.toFixed(2)} /></TableCell>
-                      </>}
-                    <TableCell align="right"><Currency value={(row.ms_rs + row.hsd_rs)?.toFixed(2)} /></TableCell>
-                  </TableRow>
-                ))
-              }
-            </TableBody>
-          </Table>
-        </div> */}
       </SalesTableWrapper>
-
     </SalesInfoWrapper>
   )
 }
-
-export default SalesInfo;
+export default MonthlySalesInfo;

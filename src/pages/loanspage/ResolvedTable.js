92910b93af550f2453e358f2363adaf643fb35a3
Shidiq Ahmed <shidiq@pentafox.in>
2021-11-17 17:53:31 +0530

    witheld download fix

--- src/pages/loanspage/ResolvedTable.js
+++ src/pages/loanspage/ResolvedTable.js
@@ -95,8 +95,26 @@ const ResolvedTable = () => {
     download: true,
     filter: true,
     isRowSelectable: () => false,
-
+    onDownload: (buildHead, buildBody, columns, data) => {
+      let Data = () => {
+        let array = []
+        data.map((item, index) => {
+          let buffer = []
+          item.data.map((data, i) => {
+            if(typeof(data) !== 'object'){
+              buffer.push(data)
+            } else {
+              let est = data.map((obj, num) => Object.values(obj)[2])
+              buffer.push(est.toString())
+            }
+          })
+          array.push({index: index+1, data: buffer})
+        })
+        return array
+      }
+      return "\uFEFF" + buildHead(columns) + buildBody(Data())
   }
+}
 
   return (
     <>

cccc44d804df96eff8b3d31abe94c0ab807c0c49
Shidiq Ahmed <shidiq@pentafox.in>
2021-12-27 14:57:17 +0530

    fix(los-filter): fuel loans stats fix

--- src/services/loans.service.js
+++ src/services/loans.service.js
@@ -6,9 +6,9 @@ export const getLoanStats = (qryStr = {}) => {
   return new Promise((resolve, reject) => {
     const { region, from, to, products, zone } = qryStr;
     let apiUrl = 'metrics/loan/stats';
-    if (zone !== '0' && zone !=='') apiUrl += apiUrl.split('?').length === 1 ?  '?' + `zone=${zone}` : '&' + `zone=${zone}`;
-    if (region !== '0' && region !=='') apiUrl += apiUrl.split('?').length === 1 ? '?' + `region=${region}` : '&' + `region=${region}`;
-    if (products !== '0' && products !=='') apiUrl += apiUrl.split('?').length === 1 ? '?' + `product=${products}` : '&' + `product=${products}`;
+    if (zone !== '0' && zone !=='' && zone) apiUrl += apiUrl.split('?').length === 1 ?  '?' + `zone=${zone}` : '&' + `zone=${zone}`;
+    if (region !== '0' && region !=='' && region) apiUrl += apiUrl.split('?').length === 1 ? '?' + `region=${region}` : '&' + `region=${region}`;
+    if (products !== '0' && products !=='' && products) apiUrl += apiUrl.split('?').length === 1 ? '?' + `product=${products}` : '&' + `product=${products}`;
     if (from && to) apiUrl = apiUrl += apiUrl.split('?').length === 1 ? '?' + `from=${from}&to=${to}` : '&' + `from=${from}&to=${to}`;
     apiCall(apiUrl)
       .then(({ status, data, message }) => {
@@ -130,9 +130,9 @@ export const getLoansByStatus = (status, filterQry) => {
   return new Promise((resolve, reject) => {
     const { region, from, to, products, zone } = filterQry;
     let apiUrl = `${URL.loans}?status=${status}`;
-    if (zone !== '0' && zone !== '') apiUrl += apiUrl.split('?').length === 1 ?  '?' + `zone=${zone}` : '&' + `zone=${zone}`;
-    if (region !== '0' && region !== '') apiUrl += apiUrl.split('?').length === 1 ? '?' + `region=${region}` : '&' + `region=${region}`;
-    if (products !== '0' && products !== '') apiUrl += apiUrl.split('?').length === 1 ? '?' + `product=${products}` : '&' + `product=${products}`;
+    if (zone !== '0' && zone !== '' && zone) apiUrl += apiUrl.split('?').length === 1 ?  '?' + `zone=${zone}` : '&' + `zone=${zone}`;
+    if (region !== '0'&& region !== 0 && region !== '' && region) apiUrl += apiUrl.split('?').length === 1 ? '?' + `region=${region}` : '&' + `region=${region}`;
+    if (products !== '0' && products !== '' && products) apiUrl += apiUrl.split('?').length === 1 ? '?' + `product=${products}` : '&' + `product=${products}`;
     if (from && to) apiUrl = apiUrl += apiUrl.split('?').length === 1 ? '?' + `from=${from}&to=${to}` : '&' + `from=${from}&to=${to}`;
     apiCall(apiUrl)
       .then(({ status, data, message }) => {

818ae0fb49325203b6ebe526abcaad486ee1d5c7
Shidiq Ahmed <shidiq@pentafox.in>
2022-01-27 10:50:04 +0530

    refactor(vehicle-details): Font revert and vehicle list ui

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -2,7 +2,7 @@ import { colors } from '@material-ui/core';
 import palette from './palette';
 
 export default {
-  fontFamily:'\'Open Sans\', Roboto, sans-serif',
+  fontFamily: '\'Nunito Sans\', \'Open Sans\', Roboto, sans-serif',
   fontWeightMedium: 600,
   h1: {
     color: palette.text.primary,

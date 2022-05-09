b9cd5955d68f5c4b2545aaa97668ee99b382f46f
PraneshS1009 <97275546+PraneshS1009@users.noreply.github.com>
2022-05-09 11:18:43 +0530

    refactor: removed comented lines

--- src/pages/dealershipDetails/PDRForms/AddCreditPdForm.js
+++ src/pages/dealershipDetails/PDRForms/AddCreditPdForm.js
@@ -1,3 +1,4 @@
+import { IconButton } from '@material-ui/core'
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -5,7 +6,6 @@ import CloseIcon from '@material-ui/icons/Close';
 import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
 import NavigateNextRounded from '@material-ui/icons/NavigateNextRounded';
 import { makeStyles } from '@material-ui/styles';
-import { IconButton } from '@material-ui/core'
 import clsx from 'clsx';
 import { useFormik } from 'formik';
 import { useSnackbar } from 'notistack';
@@ -13,13 +13,11 @@ import React from 'react';
 import * as Yup from 'yup';
 import Button from '../../../components/CommonComponents/Button/Button';
 import TextInput from '../../../components/TextInput/TextInput';
-// import { getDealershipById } from '../../../services/dealerships.service';
 import { URL } from '../../../config/serverUrls';
 
 
 const useStyles = makeStyles((theme) => ({
   sidePanelTitle: {
-    // textAlign: 'center',
     padding: '12px 16px',
     display: 'flex',
     justifyContent: 'space-between',
@@ -165,14 +163,14 @@ const AddCreditPdForm = ({ data, dealer_id, callback, currentUser, editable }) =
           </div>
           {
             !editable &&
-            <Button
-              variant="contained"
-              className={clsx(classes.btn, classes.editButton)}
-              startIcon={<NavigateNextRounded />}
-              onClick={handleSubmit}
-            >
-              Save
-            </Button>
+              <Button
+                variant="contained"
+                className={clsx(classes.btn, classes.editButton)}
+                startIcon={<NavigateNextRounded />}
+                onClick={handleSubmit}
+              >
+                Save
+              </Button>
           }
         </div>
       </div>

eab29f6fb154412e95d91cd2ff00e17337e07518
Shidiq Ahmed <shidiq@pentafox.in>
2022-05-16 16:38:06 +0530

    refactor(Opportunity-report): potentials label update

--- src/components/Tables/OpportunityReport.js
+++ src/components/Tables/OpportunityReport.js
@@ -16,77 +16,21 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
-// import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
-// import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
-// import BeenhereOutlinedIcon from '@material-ui/icons/BeenhereOutlined';
-// import CachedOutlinedIcon from '@material-ui/icons/CachedOutlined';
-// import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
-// import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
-// import { makeStyles } from '@material-ui/styles';
 import { Formik } from 'formik';
 import { head } from 'lodash';
 import React, { useEffect, useState } from 'react';
-// import { useMount } from 'react-use';
 import * as Yup from 'yup';
 import { logger } from '../../config/logger';
 import usePageTitle from '../../hooks/usePageTitle';
 import {
-  // getOpportunities,
   getPotentialOpportunity,
 } from '../../services/loans.service';
 import LoaderButton from '../CommonComponents/Button/LoaderButton';
 import { ViewData } from '../CommonComponents/FilePreview';
 import Currency from '../Number/Currency';
 
-// const useStyles = makeStyles(() => ({
-//   cardWrapper: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//     justifyContent: 'space-between',
-//   },
-//   rootCard: {
-//     flex: '2 0 21%',
-//     margin: 8,
-//     padding: 16,
-//     minWidth: 220,
-//     backgroundColor: '#FFF',
-//     borderRadius: 6,
-//     display: 'inline-block',
-//     position: 'relative',
-//     overflow: 'hidden',
-//     boxShadow:
-//       'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
-//   },
-//   cardTitle: {
-//     fontSize: '1.4rem',
-//   },
-//   content: {
-//     display: 'flex',
-//     alignItems: 'flex-end',
-//     flexDirection: 'column',
-//     marginTop: 8,
-//   },
-//   value: {
-//     fontSize: '28px',
-//     fontWeight: 'bolder',
-//   },
-//   valueTitle: {
-//     color: 'rgb(0,0,0,0.3)',
-//   },
-//   iconStyle: {
-//     position: 'absolute',
-//     color: 'rgb(0,0,0,0.03)',
-//     right: '-85px',
-//     top: '-10px',
-//     fontSize: 240,
-//     zIndex: '-1px',
-//   },
-// }));
-
 const OpportunityReport = () => {
   usePageTitle('Opportunity Report');
-  // const classes = useStyles();
-  // const [opportunities, setOpportunities] = useState();
   const [view, setView] = useState('state');
   const [potentialOpportunity, setPotentialOpportunity] = useState([]);
   const [rowData, setRowData] = useState([]);
@@ -103,14 +47,6 @@ const OpportunityReport = () => {
         logger(e);
       });
   }, [view]);
-
-  // useMount(() => {
-  //   getOpportunities()
-  //     .then(setOpportunities)
-  //     .catch((e) => {
-  //       logger(e);
-  //     });
-  // });
 
   const sortArray = (arr, orderBy, key) => {
     switch (orderBy) {
@@ -216,12 +152,13 @@ const OpportunityReport = () => {
               </LoaderButton>
             </div>
             <ViewData
-              title="Potential Opportunity (in Crs)"
+              title="Potential Opportunity"
               value={
-                <span style={{ fontSize: '1.1rem' }}>
+                <span style={{ fontSize: '1.4rem', color: '#3f51b5' }}>
                   <strong>
                     <Currency value={total?.total_average_ticket_count} />
                   </strong>
+                  {' Crs'}
                 </span>
               }
             />
@@ -319,79 +256,8 @@ const OpportunityReport = () => {
           </TableFooter>
         </Table>
       </Box>
-      {/* <div className={classes.cardWrapper}>
-        <OpportunityCard
-          title="Leads"
-          currValue={opportunities?.current?.leads}
-          projValue={opportunities?.projection?.leads}
-          icon={PersonOutlinedIcon}
-        />
-        <OpportunityCard
-          title="Convertion Count"
-          currValue={opportunities?.current?.convertion_count}
-          projValue={opportunities?.projection?.convertion_count}
-          icon={CachedOutlinedIcon}
-        />
-        <OpportunityCard
-          title="Avg. Time Taken"
-          currValue={opportunities?.current?.average_time_taken}
-          projValue={opportunities?.projection?.average_time_taken}
-          icon={AccessTimeOutlinedIcon}
-        />
-        <OpportunityCard
-          title="Rejection Count"
-          currValue={opportunities?.current?.rejection_count}
-          projValue={opportunities?.projection?.rejection_count}
-          icon={CancelOutlinedIcon}
-        />
-        <OpportunityCard
-          title="Amount Approved"
-          currValue={opportunities?.current?.amount_approved}
-          projValue={opportunities?.projection?.amount_approved}
-          currency={true}
-          icon={BeenhereOutlinedIcon}
-        />
-        <OpportunityCard
-          title="Avg. Amount"
-          currValue={opportunities?.current?.average_amount}
-          projValue={opportunities?.projection?.average_amount}
-          currency={true}
-          icon={AccountBalanceOutlinedIcon}
-        />
-      </div> */}
     </>
   );
 };
-
-// const OpportunityCard = ({
-//   title,
-//   currValue,
-//   projValue,
-//   currency = false,
-//   icon,
-// }) => {
-//   const classes = useStyles();
-//   const Icon = icon;
-//   return (
-//     <div className={classes.rootCard}>
-//       <Icon className={classes.iconStyle} />
-//       <div className={classes.cardTitle}>{title}</div>
-//       <div className={classes.content}>
-//         <span className={classes.value}>
-//           {currency ? <Currency value={currValue} /> : currValue}
-//         </span>
-//         <div className={classes.valueTitle}>Current</div>
-//       </div>
-//       <div className={classes.content}>
-//         <span className={classes.value}>
-//           {currency ? <Currency value={projValue} /> : projValue}
-//         </span>
-//         <Typography variant="body1" className={classes.valueTitle}>
-//           Projection
-//         </Typography>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default OpportunityReport;

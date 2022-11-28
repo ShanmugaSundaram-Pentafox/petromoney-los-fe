6f6404bce316b95869c74d48c8b7d0c3c2a411eb
GPSAM <gnanaprakasam@pentafox.in>
2022-11-28 15:39:51 +0530

    fix: loan stats total count issue in dashboard

--- src/pages/dashboard/components/DashboardFilter.js
+++ src/pages/dashboard/components/DashboardFilter.js
@@ -293,7 +293,7 @@ const DashboardFilter = ({ filterQry, setChartData, setTotalLoans, filterType, f
           { name: 'Approved', count: data?.approved_count, amount: data?.amount_approved },
           { name: 'Disb. Approval', count: data?.disbursement_approval_count || 0, amount: data?.amount_disbursement_approval },
           { name: 'Disb. Approved', count: data?.disbursement_approved_count || 0, amount: data?.amount_disbursement_approved },
-          { name: 'Disbursed', count: data?.disbursed_count, amount: data?.amount_disbursed },
+          { name: 'Disbursed', count: data?.disbursed_count, amount: data?.actual_amount_disbursed },
           { name: 'Rejected', count: data?.rejected_count },
         ];
         setChartData(cdata);

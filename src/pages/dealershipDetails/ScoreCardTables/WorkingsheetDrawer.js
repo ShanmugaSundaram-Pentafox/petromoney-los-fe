f5ee48da724e4a688a8858d2b364218a50c6ffc4
madhumathi_allimuthu <madhumathi@pentafox.in>
2022-12-14 11:10:10 +0530

    fix(score-card): added tooltip for the collapsible

--- src/pages/dealershipDetails/ScoreCardTables/WorkingsheetDrawer.js
+++ src/pages/dealershipDetails/ScoreCardTables/WorkingsheetDrawer.js
@@ -1,6 +1,6 @@
-import { Collapse, Paper, Typography } from '@material-ui/core';
+import { Collapse, Paper, Tooltip, Typography } from '@material-ui/core';
 import { ArrowDropDownSharp, ArrowRightOutlined } from '@material-ui/icons';
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import WorkingSheetTable from './WorkingSheetTable';
 import { getScoreCard } from '../../../services/common.service';
@@ -9,13 +9,15 @@ const WorkingSheetDrawer = ({ id }) => {
   const [collapse, setCollapse] = useState(false);
   const { data: scoreCardData = [] } = useQuery('scorecard', () => getScoreCard(id), { refetchOnWindowFocus: false })
   return (
-    <Paper variant='outlined' style={{ marginTop: 20, marginBottom: 20 }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 10 }} onClick={() => setCollapse(!collapse)}>
-        <Typography variant='h6' style={{ cursor: 'pointer' }}>Working sheet</Typography>
-        {collapse ? <ArrowDropDownSharp /> : <ArrowRightOutlined />}
-      </div>
+    <Paper variant='outlined' style={{ marginTop: 20, marginBottom: 20, cursor: 'pointer' }}>
+      <Tooltip title={!collapse ? 'click to view the working sheet' : 'click to close the working sheet'}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 10 }} onClick={() => setCollapse(!collapse)}>
+          <Typography variant='h6' style={{ cursor: 'pointer' }}>Working sheet</Typography>
+          {collapse ? <ArrowDropDownSharp /> : <ArrowRightOutlined />}
+        </div>
+      </Tooltip>
       <Collapse in={collapse}>
-        <div style={{ paddingLeft: 10, paddingBottom: 10 }}>
+        <div style={{ paddingLeft: 10, paddingBottom: 10, cursor: 'default' }}>
           <WorkingSheetTable data={scoreCardData?.working_sheet} />
         </div>
       </Collapse>

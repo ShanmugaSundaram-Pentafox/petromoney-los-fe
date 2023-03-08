c7020e2184939a48c6c2e4274291755d1d952b7b
madhumathi_allimuthu <madhumathi@pentafox.in>
2023-03-08 12:54:56 +0530

    fix(dashboard): removed unused code

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -22,7 +22,6 @@ import usePageTitle from '../../hooks/usePageTitle';
 import { getDealerDetails } from '../../services/dealers.service';
 import { getAll_ls1_Metrices, getAll_ls2_Metrices, getAllOmcDpd, getAllRegionDpd } from '../../services/loans.service';
 import { isAllowed } from '../../utils/cerbos';
-// import { getAll_ls1_Metrices, getAll_ls2_Metrices, getAllOmcDpd, getAllRegionDpd } from '../../services/loans.service';
 
 const currencyFormat = (value) => {
   const money = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', maximumSignificantDigits: 8 }).format(value)

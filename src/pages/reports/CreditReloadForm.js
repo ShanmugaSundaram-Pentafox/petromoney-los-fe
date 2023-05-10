8b7317a44ca3a45ac4e305bd0ccca0a98d525b4b
madhumathi_allimuthu <madhumathi@pentafox.in>
2023-05-10 17:30:06 +0530

    fix(CRR): updated error validation

--- src/pages/reports/CreditReloadForm.js
+++ src/pages/reports/CreditReloadForm.js
@@ -116,9 +116,13 @@ const CreditReloadForm = ({ callback, currentUser, view }) => {
       })?.filter(item => item !== undefined)
     }
   })
-  const { data: creditLimit } = useQuery(['credit-reload-limit', selectedValue], () => getCreditReloadLimitById(selectedValue), {
+  const { data: creditLimit, isError, isFetched } = useQuery(['credit-reload-limit', selectedValue], () => getCreditReloadLimitById(selectedValue), {
     refetchOnWindowFocus: false,
+    retry: false,
     enabled: selectedValue ? true : false,
+    onError: (err) => {
+      setErrorMessage(err)
+    }
   })
   useEffect(() => {
     if (selectedValue) {
@@ -129,7 +133,7 @@ const CreditReloadForm = ({ callback, currentUser, view }) => {
     }
   }, [bankData, selectedValue])
 
-  const { values, errors, handleChange, handleSubmit, isSubmitting, setSubmitting, setFieldValue, setFieldError } = useFormik({
+  const { values, errors, handleChange, handleSubmit, setFieldValue, setFieldError } = useFormik({
     initialValues: {
       amount: amount,
     },
@@ -253,157 +257,162 @@ const CreditReloadForm = ({ callback, currentUser, view }) => {
                     }
                   </Grid>
                 </Grid>
-                <Grid container spacing={2}>
-                  <Grid item md={8} style={{ marginBottom: 10 }}>
-                    <label style={{ marginBottom: 8 }}>Choose Bank</label>
-                    <Select name='bankId' isClearable value={bankId} onChange={setBankId} options={bankData} />
-                  </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                  <Grid item md={8} style={{ marginBottom: 10 }}>
-                    <label style={{ marginBottom: 8 }}>Repayment Made</label>
-                    <Select
-                      isClearable
-                      onChange={setRepaymentType}
-                      options={[
-                        { label: 'Today', value: 'today' },
-                        { label: 'Earlier today', value: 'earlier today' },
-                      ]} />
-                  </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                  <Grid item md={8}>
-                    <label>Amount</label>
-                    <TextInput
-                      money
-                      name="amount"
-                      type="number"
-                      className={classes.number}
-                      value={values.amount}
-                      error={errors.amount}
-                      helperText={errors.amount}
-                      onChange={handleChange}
-                    />
-                    <FormHelperText variant='contained'>
-                      {
-                        creditLimit &&
-                          <h3 style={{ color: 'black' }}>Available Limit: < Currency value={creditLimit} /></h3>
-                      }
-                    </FormHelperText>
-                  </Grid>
-                </Grid>
-                <Grid container spacing={2} style={{ marginTop: 11 }}>
-                  <Grid item md={12}>
-                    <Grid container spacing={1}>
-                      <Grid style={{ marginLeft: 4 }} md={12}>
-                        <label>Payment Reference</label>
+                {
+                  (!isError && isFetched) && (
+                    <>
+                      <Grid container spacing={2}>
+                        <Grid item md={8} style={{ marginBottom: 10 }}>
+                          <label style={{ marginBottom: 8 }}>Choose Bank</label>
+                          <Select name='bankId' isClearable value={bankId} onChange={setBankId} options={bankData} />
+                        </Grid>
                       </Grid>
-                      <Grid md={3}>
-                        <div className={classes.grid}>
-                          <input
-                            type='file'
-                            name='file'
-                            id='proof1'
-                            className={classes.inputFile}
-                            accept="image/jpeg,image/png,application/pdf"
-                            onChange={(e) => { onChangeHandler(e, 'proof1') }}
+                      <Grid container spacing={2}>
+                        <Grid item md={8} style={{ marginBottom: 10 }}>
+                          <label style={{ marginBottom: 8 }}>Repayment Made</label>
+                          <Select
+                            isClearable
+                            onChange={setRepaymentType}
+                            options={[
+                              { label: 'Today', value: 'today' },
+                              { label: 'Earlier today', value: 'earlier today' },
+                            ]} />
+                        </Grid>
+                      </Grid>
+                      <Grid container spacing={2}>
+                        <Grid item md={8}>
+                          <label>Amount</label>
+                          <TextInput
+                            money
+                            name="amount"
+                            type="number"
+                            className={classes.number}
+                            value={values.amount}
+                            error={errors.amount}
+                            helperText={errors.amount}
+                            onChange={handleChange}
                           />
-                          <label htmlFor='proof1'>
-                            <div style={{
-                              border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center', borderColor: errors.proof_1_file ? 'red' : 'grey'
-                            }}>
-                              {
-                                values?.proof_1_file ? (
-                                  <>
-                                    <CheckCircleTwoToneIcon style={{ color: green[300], fontSize: 30 }} />
-                                  </>
-                                ) : (
-                                  <label htmlFor='proof1' style={{ fontSize: 32, color: 'grey' }}>+</label>
-                                )
-                              }
-                            </div>
-                          </label>
-                          {
-                            values?.proof_1_file && (
-                              <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_1_file?.name}</h5>
-                            )
-                          }
-                        </div>
+                          <FormHelperText variant='contained'>
+                            {
+                              creditLimit &&
+                                <h3 style={{ color: 'black' }}>Available Limit: < Currency value={creditLimit} /></h3>
+                            }
+                          </FormHelperText>
+                        </Grid>
                       </Grid>
-                      {
-                        values.proof_1_file && (
-                          <>
+                      <Grid container spacing={2} style={{ marginTop: 11 }}>
+                        <Grid item md={12}>
+                          <Grid container spacing={1}>
+                            <Grid style={{ marginLeft: 4 }} md={12}>
+                              <label>Payment Reference</label>
+                            </Grid>
                             <Grid md={3}>
                               <div className={classes.grid}>
                                 <input
                                   type='file'
                                   name='file'
-                                  id='proof2'
-                                  accept="image/jpeg,image/png,application/pdf"
+                                  id='proof1'
                                   className={classes.inputFile}
-                                  onChange={(e) => { onChangeHandler(e, 'proof2') }}
+                                  accept="image/jpeg,image/png,application/pdf"
+                                  onChange={(e) => { onChangeHandler(e, 'proof1') }}
                                 />
-                                <label htmlFor='proof2'>
-                                  <div style={{ border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <label htmlFor='proof1'>
+                                  <div style={{
+                                    border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center', borderColor: errors.proof_1_file ? 'red' : 'grey'
+                                  }}>
                                     {
-                                      values?.proof_2_file ? (
+                                      values?.proof_1_file ? (
                                         <>
                                           <CheckCircleTwoToneIcon style={{ color: green[300], fontSize: 30 }} />
                                         </>
                                       ) : (
-                                        <label htmlFor='proof2' style={{ fontSize: 32, color: 'grey' }}>+</label>
+                                        <label htmlFor='proof1' style={{ fontSize: 32, color: 'grey' }}>+</label>
                                       )
                                     }
                                   </div>
                                 </label>
                                 {
-                                  values?.proof_2_file && (
-                                    <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_2_file?.name}</h5>
+                                  values?.proof_1_file && (
+                                    <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_1_file?.name}</h5>
                                   )
                                 }
                               </div>
                             </Grid>
                             {
-                              values.proof_2_file && (
-                                <Grid md={3}>
-                                  <div className={classes.grid}>
-                                    <input
-                                      type='file'
-                                      name='file'
-                                      id='proof3'
-                                      accept="image/jpeg,image/png,application/pdf"
-                                      className={classes.inputFile}
-                                      onChange={(e) => { onChangeHandler(e, 'proof3') }}
-                                    />
-                                    <label htmlFor='proof3'>
-                                      <div style={{ border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                        {
-                                          values?.proof_3_file ? (
-                                            <>
-                                              <CheckCircleTwoToneIcon style={{ color: green[300], fontSize: 30 }} />
-                                            </>
-                                          ) : (
-                                            <label htmlFor='proof3' style={{ fontSize: 32, color: 'grey' }}>+</label>
-                                          )
-                                        }
-                                      </div>
-                                    </label>
-                                    {
-                                      values?.proof_3_file && (
-                                        <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_3_file?.name}</h5>
-                                      )
-                                    }
-                                  </div>
-                                </Grid>
+                              values.proof_1_file && (
+                                <>
+                                  <Grid md={3}>
+                                    <div className={classes.grid}>
+                                      <input
+                                        type='file'
+                                        name='file'
+                                        id='proof2'
+                                        accept="image/jpeg,image/png,application/pdf"
+                                        className={classes.inputFile}
+                                        onChange={(e) => { onChangeHandler(e, 'proof2') }}
+                                      />
+                                      <label htmlFor='proof2'>
+                                        <div style={{ border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                          {
+                                            values?.proof_2_file ? (
+                                              <>
+                                                <CheckCircleTwoToneIcon style={{ color: green[300], fontSize: 30 }} />
+                                              </>
+                                            ) : (
+                                              <label htmlFor='proof2' style={{ fontSize: 32, color: 'grey' }}>+</label>
+                                            )
+                                          }
+                                        </div>
+                                      </label>
+                                      {
+                                        values?.proof_2_file && (
+                                          <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_2_file?.name}</h5>
+                                        )
+                                      }
+                                    </div>
+                                  </Grid>
+                                  {
+                                    values.proof_2_file && (
+                                      <Grid md={3}>
+                                        <div className={classes.grid}>
+                                          <input
+                                            type='file'
+                                            name='file'
+                                            id='proof3'
+                                            accept="image/jpeg,image/png,application/pdf"
+                                            className={classes.inputFile}
+                                            onChange={(e) => { onChangeHandler(e, 'proof3') }}
+                                          />
+                                          <label htmlFor='proof3'>
+                                            <div style={{ border: '1px dashed grey', height: 75, borderRadius: 6, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                              {
+                                                values?.proof_3_file ? (
+                                                  <>
+                                                    <CheckCircleTwoToneIcon style={{ color: green[300], fontSize: 30 }} />
+                                                  </>
+                                                ) : (
+                                                  <label htmlFor='proof3' style={{ fontSize: 32, color: 'grey' }}>+</label>
+                                                )
+                                              }
+                                            </div>
+                                          </label>
+                                          {
+                                            values?.proof_3_file && (
+                                              <h5 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', marginTop: 2, overflow: 'hidden' }}>{values.proof_3_file?.name}</h5>
+                                            )
+                                          }
+                                        </div>
+                                      </Grid>
+                                    )
+                                  }
+                                </>
                               )
                             }
-                          </>
-                        )
-                      }
-                    </Grid>
-                  </Grid>
-
-                </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </>
+                  )
+                }
               </form>
             </Box>
           </div>
@@ -419,21 +428,23 @@ const CreditReloadForm = ({ callback, currentUser, view }) => {
             </div>
             <div>
               {
-                <LoaderButton
-                  variant='contained'
-                  className={clsx(classes.btn, classes.editButton)}
-                  isLoading={loading}
-                  loadingText='Submitting...'
-                  type='submit'
-                  onClick={() => {
-                    if (repaymentType?.value == 'today') {
-                      !values?.proof_1_file ? setFieldError('proof_1_file', 'Please add proof') : handleSubmit();
-                    }
-                    else {
-                      handleSubmit();
-                    }
-                  }}
-                >Submit</LoaderButton>
+                (!isError && isFetched) && (
+                  <LoaderButton
+                    variant='contained'
+                    className={clsx(classes.btn, classes.editButton)}
+                    isLoading={loading}
+                    loadingText='Submitting...'
+                    type='submit'
+                    onClick={() => {
+                      if (repaymentType?.value == 'today') {
+                        !values?.proof_1_file ? setFieldError('proof_1_file', 'Please add proof') : handleSubmit();
+                      }
+                      else {
+                        handleSubmit();
+                      }
+                    }}
+                  >Submit</LoaderButton>
+                )
               }
             </div>
           </div>

12c19d0641a95379b9b8deaf9e0bbf749297dd6f
madhumathi_allimuthu <madhumathi@pentafox.in>
2023-06-21 13:43:34 +0530

    fix(CRR): udated default filter value and removed logs

--- src/pages/dashboard/components/CreditDashboardFilter.js
+++ src/pages/dashboard/components/CreditDashboardFilter.js
@@ -21,7 +21,7 @@ const CreditDashboardFilter = ({ filterQry, filterType, setChartData, refetch, f
   const [selectedProducts, setSelectedProducts] = useState([{ label: 'ALL', value: 0 }]);
   const [selectedZones, setSelectedZones] = useState([{ label: 'ALL', value: 0 }]);
   const [selectedPeriodType, setSelectedPeriodType] = useState(filterType == 'processed' ? 'D' : 'W');
-  const [selectedPeriod, setSelectedPeriod] = useState({});
+  const [selectedPeriod, setSelectedPeriod] = useState({ from: new Date(), to: new Date() });
   const [showPicker, setShowPicker] = useState();
   const [selectedDealership, setSelectedDealership] = useState({});
   const { enqueueSnackbar } = useSnackbar();
@@ -124,7 +124,7 @@ const CreditDashboardFilter = ({ filterQry, filterType, setChartData, refetch, f
   }
   const handleSearch = () => {
     if (filterType == 'processed') {
-      if (selectedDealership?.id)
+      if (selectedDealership?.id || selectedPeriodType == 'D')
         refetch()
       else
         setSelectedDealership({ ...selectedDealership, error: 'Please enter dealership ID to get data' })

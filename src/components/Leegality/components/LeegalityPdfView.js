a238907a26cfe0d16252910f628d7b66b5addf91
madhumathi_allimuthu <madhumathi@pentafox.in>
2023-07-11 17:39:49 +0530

    fix(score-card): added new table in PD sheet

--- src/components/Leegality/components/LeegalityPdfView.js
+++ src/components/Leegality/components/LeegalityPdfView.js
@@ -7,7 +7,7 @@ const LeegalityPdfView = ({ pdfUrl, loading }) => {
   return (
     <Grid item sm={8} md={7} style={{ position: 'relative' }}>
       {pdfUrl ? <FilePreview height="70vh" data={{ image: pdfUrl, type: pdfUrl?.endsWith('.pdf') }} /> : null}
-      <Backdrop open={loading} style={{ zIndex: '2', position: 'absolute' }}>
+      <Backdrop open={loading} style={{ zIndex: 2, position: 'absolute' }}>
         <CircularProgress size={25} style={{ color: 'white' }} />
       </Backdrop>
     </Grid>

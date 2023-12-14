da1a31a1bec555e84a63082640c91b104c0ff249
pranesh <pranesh.s@pentafox.in>
2023-12-14 15:14:10 +0530

    feat(SignRequestLayout): added the override button for all documents

--- src/components/Leegality/SignRequestLayout.js
+++ src/components/Leegality/SignRequestLayout.js
@@ -87,9 +87,25 @@ const SignRequestLayout = ({ onClose, title, type, dealershipId, loanId, callbac
   const handleResign = () => {
     deleteResignDocument(dealershipId)
       .then((data) => {
+        enqueueSnackbar('Document Override successfully', {
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          },
+          variant: 'success',
+        })
         onClose()
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        enqueueSnackbar(err?.message || 'Document Override Failed', {
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          },
+          variant: 'error',
+        })
+      })
   }
 
   useEffect(() => {
@@ -413,7 +429,7 @@ const SignRequestLayout = ({ onClose, title, type, dealershipId, loanId, callbac
               <Button variant="contained" color='primary' onClick={() => { setReinitiate(true); }}>Re-Initiate</Button>
           }
           {
-            !loansData?.is_signed && loansData?.document_id && resign && type === 'agreement' ?
+            !loansData?.is_signed && loansData?.document_id && resign ?
               <Button variant="contained" color='primary' onClick={handleResign} style={{ marginLeft: 12 }}>Override Document</Button> : null
           }
           {activeState?.invitations?.filter((item) => item?.expired)?.length > 0

404699e5f95beda0b45431bfb4be1436018bbd89
pranesh <pranesh.s@pentafox.in>
2023-12-14 14:58:49 +0530

    feat(withheld): added the conformation modal

--- src/pages/loanspage/UnResolvedTable.js
+++ src/pages/loanspage/UnResolvedTable.js
@@ -1,9 +1,10 @@
-import { Grid, Drawer, Paper, Tooltip } from '@material-ui/core';
+import { Grid, Drawer, Paper, Tooltip, Dialog, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { green } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
 import { DeleteOutlineRounded } from '@material-ui/icons';
 import CheckOutlinedIcon from '@material-ui/icons/CheckOutlined';
+import { Alert } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/styles';
 import MUIDataTable from 'mui-datatables';
 import { useSnackbar } from 'notistack';
@@ -12,6 +13,7 @@ import { useQuery, useQueryClient } from 'react-query';
 import { useMount } from 'react-use';
 import AddBlackListForm from './AddBlackListForm';
 import Button from '../../components/CommonComponents/Button/Button';
+import { TextEditor } from '../../components/TextEditor/TextEditor';
 import { action_id, resources_id } from '../../config/accessControl';
 import { getAllDealership } from '../../services/dealerships.service';
 import { deleteRemarks, getAllWithheldLoans, resolveRemarks } from '../../services/withheld.services';
@@ -28,10 +30,10 @@ const UnresolvedTable = ({ currentUser }) => {
   const queryClient = useQueryClient()
   const [openModal, setOpenModal] = useState(false);
   const [dealershipData, setDealershipData] = useState([]);
+  const [withheldModal, setWithheldModal] = useState(false);
   const classes = useStyles()
   const { enqueueSnackbar } = useSnackbar();
   const { data = [], isLoading } = useQuery('withheld-loans', () => getAllWithheldLoans(0), { refetchOnWindowFocus: false })
-
 
   useMount(() => {
     getAllDealership()
@@ -47,7 +49,7 @@ const UnresolvedTable = ({ currentUser }) => {
   })
 
   const handleResolve = (id) => {
-    resolveRemarks(id)
+    resolveRemarks(id, withheldModal?.json)
       .then(res => {
         enqueueSnackbar(res, {
           anchorOrigin: {
@@ -68,9 +70,13 @@ const UnresolvedTable = ({ currentUser }) => {
           variant: 'error',
         })
       })
+      .finally(() => {
+        setWithheldModal(false);
+      })
   }
+
   const handleDelete = (id) => {
-    deleteRemarks(id)
+    deleteRemarks(id, withheldModal?.json)
       .then(res => {
         enqueueSnackbar(res, {
           anchorOrigin: {
@@ -91,6 +97,17 @@ const UnresolvedTable = ({ currentUser }) => {
           variant: 'error',
         })
       })
+      .finally(() => {
+        setWithheldModal(false);
+      })
+  }
+
+  const handleOnclick = () => {
+    if (withheldModal?.json?.length) {
+      withheldModal?.type === 'resolve' ? handleResolve(withheldModal?.id) : handleDelete(withheldModal?.id);
+    } else {
+      setWithheldModal({ ...withheldModal, error: 'Please enter remarks' });
+    }
   }
 
   const columns = useMemo(() => {
@@ -126,7 +143,7 @@ const UnresolvedTable = ({ currentUser }) => {
         },
       },
       {
-        label: 'Remarks',
+        label: 'Reason',
         name: 'comments',
         options: {
           filter: false,
@@ -141,14 +158,14 @@ const UnresolvedTable = ({ currentUser }) => {
                   <div style={{ marginBottom: 12, display: 'flex' }} key={i}>
                     <div style={{ minWidth: 250, maxWidth: 250 }}>{remark.comment && remark.comment}</div>
                     <CheckAllowed currentUser={currentUser} resource={resources_id?.withheld} action={action_id?.withheld?.resolve}>
-                      <div onClick={() => handleResolve(remark.id)} style={{ marginLeft: 12 }}>
+                      <div onClick={() => { setWithheldModal({ modal: true, type: 'resolve', id: remark.id }) }} style={{ marginLeft: 12 }}>
                         <Tooltip title="Click to resolve">
                           <CheckOutlinedIcon style={{ color: green[200] }} fontSize={'small'} />
                         </Tooltip>
                       </div>
                     </CheckAllowed>
                     <CheckAllowed currentUser={currentUser} resource={resources_id?.withheld} action={action_id?.withheld?.delete}>
-                      <div onClick={() => { handleDelete(remark.id) }} style={{ marginLeft: 12 }}>
+                      <div onClick={() => { setWithheldModal({ modal: true, type: 'delete', id: remark.id }) }} style={{ marginLeft: 12 }}>
                         <Tooltip title='Click to delete'>
                           <DeleteOutlineRounded style={{ color: '#ff6666' }} fontSize={'small'} />
                         </Tooltip>
@@ -237,6 +254,47 @@ const UnresolvedTable = ({ currentUser }) => {
           <AddBlackListForm data={dealershipData} callback={() => setOpenModal(false)} />
         }
       </Drawer>
+      <Dialog
+        open={withheldModal?.modal}
+        onClose={() => {
+          setWithheldModal(false);
+        }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText className={classes.text}>
+            Do you want to {withheldModal?.type === 'resolve' ? 'resolve' : 'delete'} this withheld?
+          </DialogContentText>
+          <div className={classes.dialog}>
+            <DialogContentText id="approval-remarks-desc">
+              Please enter your remarks.
+            </DialogContentText>
+            <TextEditor setJSON={(e) => { setWithheldModal({ ...withheldModal, json: e }) }} toolBar={true} remarkData={withheldModal?.json} />
+            {
+              withheldModal?.error ?
+                <Alert severity="error" style={{ padding: '0px 16px' }}>{withheldModal?.error}</Alert> : null
+            }
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setWithheldModal(false);
+            }}
+          >
+            No
+          </Button>
+          <Button
+            onClick={handleOnclick}
+            variant='contained'
+            size='medium'
+            style={{ color: 'white', marginLeft: 16, marginRight: 16, backgroundColor: withheldModal?.type === 'resolve' ? green[500] : 'red' }}
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }

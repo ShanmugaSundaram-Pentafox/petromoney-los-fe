b4c77d772312dacaec27148fe323b132928149a1
Pranesh_S <pranesh.s1009@gmail.com>
2024-01-30 10:46:46 +0530

    refactor(dashboard): changed the table

--- src/components/Tables/DisbursementApprovedTable.js
+++ src/components/Tables/DisbursementApprovedTable.js
@@ -4,15 +4,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 import moment from 'moment';
-import MUIDataTable from 'mui-datatables';
 import React, { useMemo, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { NavLink as RouterLink } from 'react-router-dom';
-// import { createStructuredSelector } from 'reselect';
 import { getLoansByStatus } from '../../services/loans.service';
 import { setLoansByStatus } from '../../store/loans/loans.actions';
-import { dateCustomSort } from '../../utils/commonFunctions.util';
 import Currency from '../Number/Currency';
+import { createColumnHelper } from '@tanstack/react-table';
+import DataTableViewer from '../ReactTable/DataTableViewer';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -48,6 +47,7 @@ const useStyles = makeStyles(theme => ({
 const DisbursementApprovedTable = ({ title, loans, setLoansData, onRowClick, filterQry }) => {
   const classes = useStyles();
   const [loading, setLoading] = useState(false);
+  const columnHelper = createColumnHelper();
 
   useEffect(() => {
     setLoading(true);
@@ -61,119 +61,45 @@ const DisbursementApprovedTable = ({ title, loans, setLoansData, onRowClick, fil
       })
   }, [filterQry])
 
-  // useMount(() => {
-  //   if (!loans || !loans.length) {
-  //     setLoading(true);
-  //     getLoansByStatus('disbursement_approved')
-  //       .then(data => {
-  //         setLoansData('disbursement_approved', data);
-  //         setLoading(false);
-  //       })
-  //       .catch(e => {
-  //         setLoading(false);
-  //       })
-  //   }
-  // }, []);
-
-  const columns = useMemo(() => {
-    return [
-      {
-        label: 'Dealership Id',
-        name: 'dealership_id',
-        options: {
-          filter: false,
-          sort: true,
-          customBodyRender: value => {
-            return <RouterLink to={`/dealership/${value}`}>{value}</RouterLink>
-          }
-        }
-      },
-      {
-        label: 'Name',
-        name: 'name',
-        options: {
-          filter: false,
-          sort: true,
-          customBodyRender: (value) => {
-            return <>{value?.toUpperCase()}</>
-          },
-        }
-      },
-      {
-        label: 'Type',
-        name: 'type',
-        options: {
-          filter: true,
-          sort: true,
-          customBodyRender: value => <span className={clsx(classes.pill, classes[`pills_${value}`])}>{value}</span>
-        }
-      },
-      {
-        label: 'Region',
-        name: 'region',
-        options: {
-          filter: true,
-          sort: true,
-          customBodyRender: value => (<>{value ? value.toLowerCase().replace(/^(.)|\s+(.)/g, value => value.toUpperCase()) : '-'}</>)
-        }
-
-      },
-      {
-        label: 'Approved Amount',
-        name: 'amount_approved',
-        options: {
-          filter: false,
-          sort: true,
-          setCellProps: () => ({
-            align: 'right',
-          }),
-          customBodyRender: value => <Currency value={value} />
-        }
-      },
-      {
-        label: 'Approved Date',
-        name: 'loan_disbursement_approved_rejected_date',
-        options: {
-          filter: false,
-          sort: true,
-          setCellProps: () => ({
-            align: 'center',
-          }),
-          customBodyRender: value => {
-            return <div>
-              {value ? moment(new Date(value)).format('DD-MM-YYYY') : '-'}
-              {/* {value ? value : '-'} */}
-            </div>
-          }
-        }
-      }
-    ]
-  }, []);
-
-  const options = {
-    // filterType: 'checkbox',
-    selectableRowsHeader: false,
-    selectableRows: 'none',
-    isRowSelectable: () => false,
-    onRowClick: (rowData, { dataIndex }) => {
-      // console.log(rowData, rowMeta);
-      onRowClick(loans[dataIndex].dealership_id, loans[dataIndex], 'disbursement_approved')
-    },
-    customSort: (data, dataIndex, rowIndex) => {
-      let dateIndex = 5
-      return dateCustomSort(data, dataIndex, rowIndex, dateIndex)
-    }
-  };
+  const column = [
+    columnHelper.accessor('dealership_id', {
+      header: 'Dealership Id',
+      cell: (value) => <RouterLink to={`/dealership/${value?.getValue()}`}>{value?.getValue()}</RouterLink>
+    }),
+    columnHelper.accessor('name', {
+      header: 'Name',
+      cell: (value) => <span>{value?.getValue()?.toUpperCase()}</span>
+    }),
+    columnHelper.accessor('type', {
+      header: 'Type',
+      cell: (value) => <span className={clsx(classes.pill, classes[`pills_${value?.getValue()}`])}>{value?.getValue()}</span>
+    }),
+    columnHelper.accessor('region', {
+      header: 'Region',
+      cell: (value) => <span>{value?.getValue() ? value?.getValue()?.toLowerCase().replace(/^(.)|\s+(.)/g, value => value.toUpperCase()) : '-'}</span>
+    }),
+    columnHelper.accessor('field_officer', {
+      header: 'Field Officer',
+    }),
+    columnHelper.accessor('amount_approved', {
+      header: 'Approved Amount',
+      cell: (value) => <Currency value={value} />
+    }),
+    columnHelper.accessor('loan_disbursement_approved_rejected_date', {
+      header: 'Approved Date',
+      cell: (value) => <span>{value?.getValue() ? moment(new Date(value?.getValue())).format('DD-MM-YYYY') : '-'}</span>
+    }),
+  ]
 
   return (
     <div className={classes.root}>
       {
         Array.isArray(loans) && loans.length ? (
-          <MUIDataTable
-            title={title ? <Typography className={classes.title} variant="h4" component="h4">{title} ({loans.length})</Typography> : null}
-            data={loans}
-            columns={columns}
-            options={options}
+          <DataTableViewer
+            rowData={loans}
+            column={column}
+            title={`${title} (${loans.length})`}
+            onRowClick={(i) => onRowClick(i.dealership_id, i, 'disbursement_approval')}
           />
         ) : (!loading && <Paper style={{ padding: 10 }}>No pending Disbursement Approved applications</Paper>)
       }

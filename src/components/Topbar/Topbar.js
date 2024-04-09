b48f250f6994cf3dedd23e10d6e61af287e6635d
Pranesh_S <pranesh.s1009@gmail.com>
2024-04-09 22:00:00 +0530

    refactor(TopBar): changed the url

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -178,8 +178,8 @@ const Topbar = (props) => {
             </Menu.Target>
             <Menu.Dropdown>
               <Menu.Label>Our Other Services</Menu.Label>
-              <Menu.Item onClick={() => window.open('https://lms-uat.petromoney.in/', '_self')}><Badge leftSection={<IconExternalLink style={{ width: 14, height: 14 }} />} variant='light' color='blue'>LMS</Badge></Menu.Item>
-              <Menu.Item onClick={() => window.open('https://ddms-uat.petromoney.in/', '_self')}><Badge leftSection={<IconExternalLink style={{ width: 14, height: 14 }} />} variant='light' color='green'>DDMS</Badge></Menu.Item>
+              <Menu.Item onClick={() => window.open('https://lms.petromoney.in/', '_self')}><Badge leftSection={<IconExternalLink style={{ width: 14, height: 14 }} />} variant='light' color='blue'>LMS</Badge></Menu.Item>
+              <Menu.Item onClick={() => window.open('https://ddms.petromoney.in/', '_self')}><Badge leftSection={<IconExternalLink style={{ width: 14, height: 14 }} />} variant='light' color='green'>DDMS</Badge></Menu.Item>
             </Menu.Dropdown>
           </Menu>
           <Box

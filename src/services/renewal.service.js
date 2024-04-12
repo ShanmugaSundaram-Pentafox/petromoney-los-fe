b5fc6465d89aff8d3cc8f3a2d577a1c24261d81b
Pranesh_S <pranesh.s1009@gmail.com>
2024-04-12 17:35:07 +0530

    refactor(Enhancement): changed the filter key

--- src/services/renewal.service.js
+++ src/services/renewal.service.js
@@ -36,7 +36,7 @@ export const getStatusWiseRecordCount = (filterType, filterQry) => {
     if (zone && zone !== '0') qry.push(`zone=${zone}`)
     if (month && month !== '0') qry.push(`renewal_month=${month}`)
     if (region && region !== '0') qry.push(`region=${region}`)
-    if (products && products !== '0') qry.push(filterType == 'enhancement' ? `old_product_id=${products}` : `product=${products}`)
+    if (products && products !== '0') qry.push(`product=${products}`)
     if (from && to) qry.push(`from=${from}&to=${to}`)
     if (qry.length) apiUrl += '?' + qry.join('&')
 
@@ -188,7 +188,7 @@ export const sendRenewalReminder = (status) => {
   });
 }
 
-export const syncRenewalData = ({renewal_application_id}) => {
+export const syncRenewalData = ({ renewal_application_id }) => {
   return new Promise((resolve, reject) => {
     let apiUrl = `renewal/${renewal_application_id}/sync`
     apiCall(apiUrl)
@@ -205,7 +205,7 @@ export const syncRenewalData = ({renewal_application_id}) => {
   });
 }
 
-export const getRenewalFeeStatus = ({dealership_id}) => {
+export const getRenewalFeeStatus = ({ dealership_id }) => {
   return new Promise((resolve, reject) => {
     let apiUrl = `renewal/${dealership_id}/fee-status`
     apiCall(apiUrl)

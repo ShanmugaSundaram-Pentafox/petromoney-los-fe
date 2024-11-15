a4fdf6aaa730163619bdd1b0628afb46d9d0c5d5
Khavin <khavin.s@pentafox.in>
2024-11-15 17:19:55 +0530

    fix(UsersTable): pagination for users

--- src/pages/users/components/UsersTable.js
+++ src/pages/users/components/UsersTable.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink as RouterLink } from 'react-router-dom';
 import { action_id, resources_id } from '../../../config/accessControl';
 import { isAllowed } from '../../../utils/cerbos';
@@ -6,8 +6,28 @@ import DataTableViewer from '../../../components/ReactTable/DataTableViewer';
 import { Paper, Tooltip } from '@mantine/core';
 import { IconCheck, IconEdit, IconX } from '@tabler/icons-react';
 import AddNewUserAction from '../../../components/AddNewUser/AddNewUserAction';
+import { useDebouncedState } from '@mantine/hooks';
+import { useQuery } from 'react-query';
+import { getUsersData } from '../../../services/users.service';
+import usePageTitle from '../../../hooks/usePageTitle';
 
-const UsersTable = ({ title, data, withRole, currentUser, loading, refetchQuery }) => {
+const UsersTable = ({ title, withRole, currentUser, refetchQuery, apiFilter, setApiFilter }) => {
+  usePageTitle('Users');
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useDebouncedState('', 500);
+  const [downloadLoading, setDownloadLoading] = useState(false);
+  const [dateObj, setDateObj] = useState({ from: new Date(), to: new Date() });
+  const apiFilterHeader = [
+    {name:'role_id', label: 'Role', apiUrl: 'user/roles', data: null, type: 'select',},
+    {name:'status', label: 'Status', apiUrl: null, data: [{label: 'Active', value: '1'},{label: 'Inactive', value: '0'}], type: 'select'}
+  ];
+  const { data: usersData = [], isFetching } = useQuery(['users-data', search, dateObj, page, apiFilter], () => getUsersData({ search, dateObj, page, apiFilter }), {
+    refetchOnWindowFocus: false
+  });
+
+  useEffect(() => {
+    page != 1 && setPage(1)
+  }, [search,dateObj])
 
   const column = [
     {
@@ -74,8 +94,18 @@ const UsersTable = ({ title, data, withRole, currentUser, loading, refetchQuery 
   return (
     <Paper>
       <DataTableViewer
-        rowData={data}
-        loading={loading}
+        useAPIPagination
+        title={title}
+        totalNoOfRecords={usersData?.total_records}
+        rowData={usersData?.data}
+        loading={isFetching}
+        page={page}
+        setPage={setPage}
+        apiFilter={apiFilter}
+        setApiFilter={setApiFilter}
+        apiFilterHeader={apiFilterHeader}
+        totalNoOfPages={usersData?.total_pages}
+        apiSearch={setSearch}
         column={
           withRole ?
             [
@@ -89,9 +119,8 @@ const UsersTable = ({ title, data, withRole, currentUser, loading, refetchQuery 
               column
         }
         action={
-          <AddNewUserAction currentUser={currentUser} refetchQuery={refetchQuery} />
+          <AddNewUserAction currentUser={currentUser} refetchQuery={usersData?.refetch} />
         }
-        title={title}
       />
     </Paper>
   )

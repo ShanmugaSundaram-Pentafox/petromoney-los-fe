d32745832be404bb7fe35fe1f6e5ad1c087487e3
Khavin <khavin.s@pentafox.in>
2025-02-11 13:35:58 +0530

    refactor: removed unused enqueueSnackbar

--- src/pages/dealershipDetails/components/DealerCreditInfoForm.js
+++ src/pages/dealershipDetails/components/DealerCreditInfoForm.js
@@ -2,7 +2,6 @@ import { Alert, Grid, Text } from '@mantine/core';
 import { CircularProgress, Collapse } from '@material-ui/core';
 import CheckCircleOutlineOutlinedIcon from '@material-ui/icons/CheckCircleOutlineOutlined';
 import { IconBrandSpeedtest, IconInfoCircle } from '@tabler/icons-react';
-import { useSnackbar } from 'notistack';
 import React, { useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { ViewData } from '../../../components/CommonComponents/FilePreview';
@@ -24,7 +23,6 @@ const DealerCreditInfoForm = ({ values, errors, onChange, editMode, dealerData, 
   const [panLoading, setPanLoading] = useState({icon: false})
   const [cibilLoading, setCibilLoading] = useState({ icon: false })
   const [cibilData, setCibilData] = useState()
-  const { enqueueSnackbar } = useSnackbar();
   const [errorMsg, setErrorMsg] = useState({})
   const formData = new FormData()
   const [collapseOpen, setCollapseOpen] = useState(false)
